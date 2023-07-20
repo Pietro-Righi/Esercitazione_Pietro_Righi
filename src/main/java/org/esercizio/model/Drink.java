167122a8ad832506eab0fd3264a6438675045dc6
Pietro <gr2353@gmail.com>
2023-07-20 12:53:56 +0200

    Exception

--- src/main/java/org/esercizio/model/Drink.java
+++ src/main/java/org/esercizio/model/Drink.java
@@ -1,11 +1,21 @@
 package org.esercizio.model;
 
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
+@Entity
+@Table(name = "DRINK_TABLE")
 public class Drink {
 	
 
 	//La classe è una soluzione per ottenere i dati di metodi POST del controller, visto che i dati json sono salvati a loro volta 
 	//in un oggetto, si passa un contenitore simile per raccogliere il dato
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column
 	private String name;
 
 	
